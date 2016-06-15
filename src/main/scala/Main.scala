345f46765b345d0488d393ee37e216c0da73e570
Will Currie <will@currie.id.au>
2016-06-15 13:57:14 +1000

    Delete unused code

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -38,23 +38,4 @@ object Main {
 
   def countByState(threads: Seq[Thread]): Map[String, Int] = threads.groupBy(_.state).mapValues(_.size)
 
-  def printBasic(threads: Seq[Thread]): Unit = {
-    for ((stack, threads) <- threads.groupBy(_.stack).toSeq.sortBy(_._2.size).reverse) {
-      val start = stack.take(2).mkString(", ")
-      val states = threads.map(_.state).toSet.mkString("{", ", ", "}")
-      println(s"${threads.size}\t$states\t$start")
-    }
-  }
-
-  def printTree(root: Thread, edges: Set[(Thread, Thread)], indent: String = ""): Unit = {
-    println(formatThread(root, indent))
-    val nextIndent = indent + "  "
-    for ((src, dest) <- edges if dest == root) {
-      printTree(src, edges, nextIndent)
-    }
-  }
-
-  def formatThread(thread: Thread, indent: String = ""): String = {
-    s"$indent${thread.name} ${thread.stack.head} ${thread.state}"
-  }
 }
